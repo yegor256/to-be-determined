20a278cec1f10b0b0a83ff15eaccb832b2de9026
rultor <me@rultor.com>
2024-10-11 10:46:20 +0000

    version set to 0.3.1

--- build.lua
+++ build.lua
@@ -28,7 +28,7 @@ tagfiles = {"build.lua", "to-be-determined.dtx"}
 
 uploadconfig = {
   pkg = "to-be-determined",
-  version = "0.0.0",
+  version = "0.3.1",
   author = "Yegor Bugayenko",
   uploader = "Yegor Bugayenko",
   email = "yegor256@gmail.com",
